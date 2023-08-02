a8bb15aafa8269ce28871d269e8dc603c84ae0e8
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-08-02 16:48:16 +0200

    add simple controllers

---
+++ src/user/user.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getAllUsers() {
+    return await this.userService.getAll();
+  }
+
+  @Get('/:id')
+  async getUser(@Param('id') id: string) {
+    return await this.userService.getUserById(id);
+  }
+
+  @Post()
+  async createUser(@Body() user: CreateUserDto) {
+    return await this.userService.createUser(user);
+  }
+
+  @Post('/:id')
+  async deleteUser(@Param('id') id: string) {
+    return await this.userService.deleteUser(id);
+  }
+}
