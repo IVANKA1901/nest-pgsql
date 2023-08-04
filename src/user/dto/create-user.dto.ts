ca4e771020e6be4919e5707d96214bf693836d4c
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-08-04 14:44:30 +0200

    add validators

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,8 +1,32 @@
+import {
+  IsString,
+  IsNotEmpty,
+  Length,
+  IsNumber,
+  IsBoolean,
+  IsOptional,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(2, 15)
   public username: string;
+
+  @IsString()
+  @IsNotEmpty()
   public email: string;
+
+  @IsNumber()
+  @IsNotEmpty()
   public age: number;
+
+  @IsBoolean()
+  @IsOptional()
   public status: boolean;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(2, 15)
   public password: string;
-  public created_at: Date;
 }
