d372c97de13b1cc8645eafc9844a5f453b62a772
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-08-05 16:18:33 +0200

    use guards decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { AuthGuard } from 'src/auth/guards/jwt-auth-guard';
 // import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
@@ -9,21 +10,25 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @UseGuards(AuthGuard)
   async getAllUsers() {
     return await this.userService.getAll();
   }
 
   @Get('/:id')
+  @UseGuards(AuthGuard)
   async getUser(@Param('id') id: string) {
     return await this.userService.getUserById(id);
   }
 
   @Post()
+  @UseGuards(AuthGuard)
   async createUser(@Body() user: CreateUserDto) {
     return await this.userService.createUser(user);
   }
 
   @Post('/:id')
+  @UseGuards(AuthGuard)
   async deleteUser(@Param('id') id: string) {
     return await this.userService.deleteUser(id);
   }
