c4e8f01f41d74be185cd06c71a9cb61910827a5c
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-08-05 16:18:18 +0200

    update providers

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaService } from 'src/core/prisma.service';
+import { AuthGuard } from 'src/auth/guards/jwt-auth-guard';
 
 @Module({
   imports: [UserModule],
-  providers: [UserService, PrismaService],
+  providers: [UserService, PrismaService, AuthGuard, JwtService],
   controllers: [UserController],
 })
 export class UserModule {}
