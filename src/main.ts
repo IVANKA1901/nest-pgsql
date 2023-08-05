40b3f9b2cd963707c3ee0244108fc043b476eb8d
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-08-05 18:35:21 +0200

    fix the issue

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // const swaggerDescription = await fs.readdirSync(
+  //   join(__dirname, '..', 'swagger-description.markdown'),
+  // );
+
+  const config = new DocumentBuilder()
+    .setTitle('Users')
+    .setDescription('The users API description')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/doc', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3003);
+  await app.listen(5000);
 }
 bootstrap();
