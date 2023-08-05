0cb67acde1eaf845195c1c71a77b012411278bb3
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-08-05 18:34:44 +0200

    add swagger example for users

---
+++ src/core/swagger/swagger-examples/swagger-example-user.list.ts
@@ -0,0 +1,42 @@
+export const SWAGGER_EXAMPLE_CREATE_USER = {
+  id: 1,
+  username: 'Ivanka',
+  email: 'ivanka@gmail.com',
+  age: 22,
+  status: true,
+  password: '123456',
+  created_at: '2023-08-03T20:52:58.078Z',
+};
+
+export const SWAGGER_EXAMPLE_GET_ALL_USERS = [
+  {
+    id: 1,
+    username: 'Ivanka',
+    email: 'ivanka@gmail.com',
+    age: 22,
+    status: true,
+    password: '123456',
+    created_at: '2023-08-03T20:52:58.078Z',
+  },
+  {
+    id: 1,
+    username: 'Ivanka',
+    email: 'ivanka@gmail.com',
+    age: 22,
+    status: true,
+    password: '123456',
+    created_at: '2023-08-03T20:52:58.078Z',
+  },
+];
+
+export const SWAGGER_EXAMPLE_GET_USER_BY_ID = {
+  id: 1,
+  username: 'Ivanka',
+  email: 'ivanka@gmail.com',
+  age: 22,
+  status: true,
+  password: '123456',
+  created_at: '2023-08-03T20:52:58.078Z',
+};
+
+export const SWAGGER_EXAMPLE_USER_DELETE = {};
